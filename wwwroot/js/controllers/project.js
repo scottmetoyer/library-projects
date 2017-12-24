7443a4331033a703a4773ffa95d97f17cbf4d007
Scott Metoyer <scott.metoyer@gmail.com>
2017-12-24 13:48:31 -0800

    Started working create project

--- wwwroot/js/controllers/project.js
+++ wwwroot/js/controllers/project.js
@@ -6,11 +6,22 @@
   function ProjectCtrl($http, $stateParams) {
     var self = this;
     self.project = {};
+    self.formData = {};
 
-    $http.get("https://nlm8zahqm4.execute-api.us-west-1.amazonaws.com/test/projects/" + $stateParams.key)
-    .then(function(response) {
-      self.project = response.data.Item;
-    });
+    self.createProject = function() {
+      $http.post("https://nlm8zahqm4.execute-api.us-west-1.amazonaws.com/test/projects/")
+      .then(function(response) {
+        self.project = response.data.Item;
+      });
+    }
+
+    // Load up a project if we have passed a key in the state parameters
+    if ($stateParams.key != '') {
+      $http.get("https://nlm8zahqm4.execute-api.us-west-1.amazonaws.com/test/projects/" + $stateParams.key)
+      .then(function(response) {
+        self.project = response.data.Item;
+      });
+    }
   }
 
   angular.module('pixeladmin')
