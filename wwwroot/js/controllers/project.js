7fc689bc03a774874c45b13d45d4f178a3b4c212
Scott Metoyer <scott.metoyer@gmail.com>
2017-12-27 07:45:53 -0800

    Got project creation working

--- wwwroot/js/controllers/project.js
+++ wwwroot/js/controllers/project.js
@@ -3,16 +3,23 @@
   // Controllers / Project
   //
 
-  function ProjectCtrl($http, $stateParams) {
+  function ProjectCtrl($http, $stateParams, $anchorScroll) {
     var self = this;
     self.project = {};
     self.formData = {};
 
-    self.createProject = function() {
-      $http.post("https://nlm8zahqm4.execute-api.us-west-1.amazonaws.com/test/projects", JSON.stringify(self.formData))
-      .then(function(response) {
-        console.log(response.data);
-      });
+    self.createProject = function(isValid) {
+      if (isValid) {
+        $http.post(
+          "https://nlm8zahqm4.execute-api.us-west-1.amazonaws.com/test/projects",
+          JSON.stringify(self.formData))
+        .then(function(response) {
+          console.log(response);
+        }, function(response){
+          $anchorScroll();
+          self.hasError = true;
+        });
+      }
     }
 
     // Load up a project if we have passed a key in the state parameters
@@ -20,6 +27,9 @@
       $http.get("https://nlm8zahqm4.execute-api.us-west-1.amazonaws.com/test/projects/" + $stateParams.key)
       .then(function(response) {
         self.project = response.data.Item;
+      }, function(response){
+        $anchorScroll();
+        self.hasError = true;
       });
     }
   }
